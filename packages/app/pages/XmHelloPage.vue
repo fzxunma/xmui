9d0e480b81b819edb3b541ff0ff99a3ad8933e20
fzxunma <fzxunma@hotmail.com>
2025-07-22 17:48:57 +0800

    ui

--- packages/app/pages/XmHelloPage.vue
+++ packages/app/pages/XmHelloPage.vue
@@ -6,5 +6,5 @@ const increment = () => {
 };
 </script>
 <template>
-  <div @click="increment">Count撒旦杀杀杀dd发vue is: {{ count }}</div>
+  <div @click="increment" class="mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">Count撒旦杀杀杀dd发vue is: {{ count }}</div>
 </template>
